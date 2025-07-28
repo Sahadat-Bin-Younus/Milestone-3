c2496011fac98eb283e14974fed278eff3bfdee6
Sahadat-Bin-Younus <sahadat1726@gmail.com>
2025-07-28 19:24:16 +0600

    solving problem 3 in more ways

--- module 1/practiceProblem_3.js
+++ module 1/practiceProblem_3.js
@@ -12,8 +12,28 @@ print the reminder.
 
 */
 
+//one line way
  var num= 124;
+  console.log(num % 5);
+
+//2nd way
+  var num= 124;
    var numResult= 124%5;
     console.log(numResult);
 
-    
+// 3rd way
+var num = 124;
+var divisor = 5;
+var remainder = num % divisor;
+console.log(remainder);
+
+
+//more advance way   
+var num = 124;
+var quotient = Math.floor(num / 5);
+ /* Math.floor() is used when you want to extract the whole number part of the division—ignoring any decimal values. In other words, it gives you the integer quotient. here quotient var output is : 24
+ */ 
+var remainder = num % 5;
+
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
