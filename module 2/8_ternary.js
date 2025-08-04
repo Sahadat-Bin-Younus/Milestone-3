2b9449bca05f991cc7076a6b0a9d48e5ef44c735
Sahadat-Bin-Younus <sahadat1726@gmail.com>
2025-08-04 16:49:04 +0600

    ternary , logical not operator , nested ternary practiced

--- module 2/8_ternary.js
+++ module 2/8_ternary.js
@@ -1,6 +1,80 @@
 // 14_8 (advanced )If-else shorthand Ternary Operator
+/* ternary--> three parts
+  ? :
+condition ? do something when true:
+do something when false
 
- const age = 20;
+*/
+
+ 
+ 
+/*  
+
+//normal if-else
+  const age = 112;
   if (age >= 18){
     console.log("You can vote")
   }
+  else {
+    console.log("Ghumaye thako")
+  }
+
+// simpleternary operator
+age >= 18 ? console.log("vote Dio") : console.log("Ghumaye thako");
+
+ 
+
+ */
+
+/* let price = 12000;
+ const isLeader = false;
+
+ if (isLeader === true) {
+  price = 0;
+ }
+  else {
+    price = price + 100;
+  }
+  console.log(price);
+
+
+  2nd simple ternary operator
+  price = isLeader === true ? 0 : price + 100;
+  console.log(price);
+ */
+
+  // 3rd ternary(optional): semi advanced
+  let price = 1000;
+    const isLeader = false;
+
+ if(isLeader === true){
+  if (price > 1000) {
+    price = price / 2;
+      console.log(price);
+  }
+  else{
+    price = 0;
+    console.log(price);
+  }
+
+ }
+ else{
+  price = price + 100;
+  console.log(price);
+ }
+
+
+// feel free to ignore below code
+ price = isLeader === true ? 
+    price > 1000 ? 
+        price / 2 : 0 : 
+            price + 100;
+ console.log(price);
+
+
+//2nd way to write the same code
+price = (isLeader === true) ?
+   (price > 1000 ? price / 2 : 0)
+      : (price + 100);
+
+      console.log(price);
---
+++ module 2/9_shortcut.js
@@ -0,0 +1,23 @@
+// 14_9 (advanced) Logical Not Operator
+
+/* 
+
+const isLeader = true;
+if (isLeader === true) {
+ console.log(
+    "Leader asche! Chair Charo"
+);
+} else {
+    console.log("Vitore Dhukte dibi nah");
+} 
+// if (isLeader) will directly give output without executing if block
+
+*/
+
+ const isPassed = false;
+  if (!isPassed ) {
+    console.log("Toke biya diya dimu");
+  }
+  else{
+    console.log("Tumi amar noyoner moni");
+  }
